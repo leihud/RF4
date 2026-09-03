efb79a091123e7a882079d1ab16c9c7cc1a891a3
leihud <804632879@11.com>
2026-09-03 16:14:45 +0800

    chore: 自动更新版本信息

--- src/version-info.js
+++ src/version-info.js
@@ -4,9 +4,16 @@
  * 版本号格式：V+ 发布日期 + 分支代码 (如 V20260817-01)
  */
 export const versionInfo = {
-  currentVersion: 'V20260903-11',
+  currentVersion: 'V20260903-12',
   buildTime: '2026-09-03',
   changelog: [
+    {
+      version: 'V20260903-12',
+      date: '2026-09-03',
+      changes: [
+        '优化：装备数据加载添加客户端内存缓存，页面切换5分钟内不重复请求API'
+      ]
+    },
     {
       version: 'V20260903-11',
       date: '2026-09-03',
