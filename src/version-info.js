6d94a6a91b8f5e7527eacb37b33f1479f2e6b38b
leihud <804632879@11.com>
2026-09-03 15:06:09 +0800

    chore: 自动更新版本信息

--- src/version-info.js
+++ src/version-info.js
@@ -4,9 +4,16 @@
  * 版本号格式：V+ 发布日期 + 分支代码 (如 V20260817-01)
  */
 export const versionInfo = {
-  currentVersion: 'V20260903-05',
+  currentVersion: 'V20260903-06',
   buildTime: '2026-09-03',
   changelog: [
+    {
+      version: 'V20260903-06',
+      date: '2026-09-03',
+      changes: [
+        '修复：对比表格改用原生table元素彻底解决列对齐'
+      ]
+    },
     {
       version: 'V20260903-05',
       date: '2026-09-03',
