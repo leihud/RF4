f3fa31b898581066ca7ed1de73c494a3cb2f1ebc
leihud <804632879@11.com>
2026-09-03 16:09:04 +0800

    chore: 自动更新版本信息

--- src/version-info.js
+++ src/version-info.js
@@ -4,9 +4,16 @@
  * 版本号格式：V+ 发布日期 + 分支代码 (如 V20260817-01)
  */
 export const versionInfo = {
-  currentVersion: 'V20260903-10',
+  currentVersion: 'V20260903-11',
   buildTime: '2026-09-03',
   changelog: [
+    {
+      version: 'V20260903-11',
+      date: '2026-09-03',
+      changes: [
+        '修复：grid-template-columns改用计算属性内联style，避免CSS变量兼容问题'
+      ]
+    },
     {
       version: 'V20260903-10',
       date: '2026-09-03',
