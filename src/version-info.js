833bd68201dc74910f4acd55ab8a777a3f4aa528
leihud <804632879@11.com>
2026-09-03 15:43:36 +0800

    chore: 自动更新版本信息

--- src/version-info.js
+++ src/version-info.js
@@ -4,9 +4,16 @@
  * 版本号格式：V+ 发布日期 + 分支代码 (如 V20260817-01)
  */
 export const versionInfo = {
-  currentVersion: 'V20260903-08',
+  currentVersion: 'V20260903-09',
   buildTime: '2026-09-03',
   changelog: [
+    {
+      version: 'V20260903-09',
+      date: '2026-09-03',
+      changes: [
+        '修复：对比表格列对齐 - 使用table-layout:fixed强制等宽列'
+      ]
+    },
     {
       version: 'V20260903-08',
       date: '2026-09-03',
